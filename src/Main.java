bef584b6fae064ef2cb2eb78935c53d1f26f94f2
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-06-12 09:44:10 -0300

    Revert "FIX: normalize vector center in the loop"
    This reverts commit dc49c7e50c6a711d2e631597125556f995f4bc68.

--- src/Main.java
+++ src/Main.java
@@ -52,7 +52,7 @@ public class Main {
                 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
         });
 
-        Matrix result = PageRank.hitsAuthority(m2);
+        Matrix result = PageRank.hitsAuthority(q8);
 
         // por operarmos matrizes com float a precisao precisa ser ajustada por conta da baixa precisao do tipo float
         Format formatted = new Format(result, 5, SortMatrix.DEFAULT);
--- src/PageRank.java
+++ src/PageRank.java
@@ -41,6 +41,7 @@ public class PageRank {
                 onesMatrix(transposeAdj.getRows())
         );
 
+        Matrix a0Normalize = (Matrix) MatrixOperators.times(1 / norm(a0), a0);
 
 
 
@@ -49,7 +50,6 @@ public class PageRank {
         int iter = 1000;
 
         for(int i = 0; i < iter; i++){
-            Matrix a0Normalize = (Matrix) MatrixOperators.times(1 / norm(a0), a0);
             Matrix an = ponderNormalize(MatrixOperators.dot(transposeAdj, matrixAdj), a0Normalize);
 
             Matrix err = MatrixOperators.sum(an, (Matrix) MatrixOperators.times(-1f, a0Normalize));
@@ -63,7 +63,7 @@ public class PageRank {
                 break;
             }
 
-            a0 = an;
+            a0Normalize = an;
         }
 
 
